099ac621175ac8886e7919550cc1ad509b82a6f4
Forsakenftw <leoxzayn@gmail.com>
2025-08-20 09:56:50 +0530

    My local changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Shivaji" });
+  res.render("index", { title: "Rent Ease" });
 });
 
 module.exports = router;
